0cc3999dc2506d4070903b4c22b28d17839a929c
Benir2005 <beniromenda@gmail.com>
2025-03-12 22:39:46 +0300

    Sorting data

---
+++ src/benir/collections/Customer.java
@@ -0,0 +1,18 @@
+package benir.collections;
+
+public class Customer implements Comparable<Customer> {
+    private String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(Customer o) {
+        return name.compareTo(o.name);
+    }
+    @Override
+    public String toString(){
+        return name;
+    }
+}
--- src/benir/collections/Main.java
+++ src/benir/collections/Main.java
@@ -1,7 +1,16 @@
 package benir.collections;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        CollectionsDemo.show();
+        List<Customer> customers= new ArrayList<>();
+        customers.add(new Customer("Alicia"));
+        customers.add(new Customer("Becker"));
+        customers.add(new Customer("Kylian"));
+        Collections.sort(customers);
+        System.out.println(customers);
     }
 }

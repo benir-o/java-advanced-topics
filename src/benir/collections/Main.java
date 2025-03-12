332cb7b754625cd9bc2e429ec02710c4d859aa3d
Benir2005 <beniromenda@gmail.com>
2025-03-12 22:56:14 +0300

    Comparator Interface

--- src/benir/collections/Main.java
+++ src/benir/collections/Main.java
@@ -7,10 +7,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Customer> customers= new ArrayList<>();
-        customers.add(new Customer("Alicia"));
-        customers.add(new Customer("Becker"));
-        customers.add(new Customer("Kylian"));
-        Collections.sort(customers);
+        customers.add(new Customer("Alicia", "alicia@gmail.com"));
+        customers.add(new Customer("Becker", "becker@gmail.com"));
+        customers.add(new Customer("Kylian", "kylian@gmail.com"));
+        Collections.sort(customers,new EmailComparator());
         System.out.println(customers);
     }
 }

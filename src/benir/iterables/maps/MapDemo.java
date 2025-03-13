b722b90c2cf23caf43088d1f05d1caa276ec0ce3
Benir2005 <beniromenda@gmail.com>
2025-03-14 00:14:50 +0300

    Brute Force search Algorithm

---
+++ src/benir/iterables/maps/MapDemo.java
@@ -0,0 +1,16 @@
+package benir.iterables.maps;
+
+import benir.collections.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapDemo {
+    public static void show(){
+        List<Customer> customers= new ArrayList<>();
+        for (var customer: customers)
+            if (customer.getEmail()=="e1")
+                System.out.println("Found!");
+
+    }
+}

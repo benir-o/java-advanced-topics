77304bde07c435836ca56c01f72294ee655a85ec
Benir2005 <beniromenda@gmail.com>
2025-03-23 22:30:18 +0300

    Lambda Expressions introduction

--- src/benir/lambdaExpressions/LambdaDemo.java
+++ src/benir/lambdaExpressions/LambdaDemo.java
@@ -1,11 +1,17 @@
 package benir.lambdaExpressions;
 
 public class LambdaDemo {
+    public String prefix="-";
+
     public static void show(){
-        greet(new ConsolePrinter());
+        greet(message-> System.out.println(message));
     }
     public static void greet(Printer printer){
         printer.print("Hello World");
     }
+    //What is an anonymous inner class
+    //Sometimes we do not need to explicitly create a class to implement
+    //an interface
+    //Lambda expression-> An anonymous function
 
 }

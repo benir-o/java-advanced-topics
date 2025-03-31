e66b248b1e83a321cc1bfb556897661b2d30971f
Benir2005 <beniromenda@gmail.com>
2025-03-31 14:32:52 +0300

    Peek, find, sort and reduce

--- src/benir/streams/_Things.java
+++ src/benir/streams/_Things.java
@@ -1,5 +1,6 @@
 package benir.streams;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
@@ -18,7 +19,7 @@ public class _Things {
 //                .forEach(name-> System.out.println(name));
 
 
-        _Things.filter();
+        _Things.reducers();
     }
     static class Movie{
         private String name;
@@ -72,4 +73,100 @@ public class _Things {
                 .map(n->n*2)
                 .forEach(System.out::println);
     }
+    public static void streamSlice(){
+        var movies = List.of(
+                new Movie("a",10),
+                new Movie("b",20),
+                new Movie("c",30)
+
+        );
+        //100 movies
+        //10 movies per page
+        //3rd page
+        //skip 20 movies to get to the third page.
+        // position-> (page-1)* pageSize=10;
+        //position-> limit(10).... for (i in range(10))|->print (movie.getName())
+        movies
+                .stream()
+                .limit(2)
+                .forEach(m-> System.out.println(m.getName()));
+    }
+    public static void streamSort(){
+        var movies = List.of(
+                new Movie("b",10),
+                new Movie("a",20),
+                new Movie("c",30)
+
+        );
+        movies.stream()
+                .sorted(Comparator.comparing(Movie::getName).reversed())
+                .forEach(n-> System.out.println(n.getName()));
+    }
+    public static void uniqueElements(){
+        var movies = List.of(
+                new Movie("a",10),
+                new Movie("a",10),
+                new Movie("b",30),
+                new Movie("c",10)
+
+                //Display the price of the movie: likes are taken as the price
+
+        );
+        movies
+                .stream()
+                .map(Movie::getLikes)
+                .distinct()//removes duplicate values
+                .forEach(System.out::println);
+    }
+    public static void peekElements(){
+        var movies = List.of(
+                new Movie("a",10),
+                new Movie("b",20),
+                new Movie("c",30)
+
+        );
+        movies
+                .stream()
+                .filter(m->m.getLikes()>10)
+                .peek(m-> System.out.println("filtered: "+m.getName()))
+                .map(Movie::getName)
+                .peek(t-> System.out.println("mapped: "+ t))
+                .forEach(System.out::println);
+    }
+    public static void reducers(){
+        var movies = List.of(
+                new Movie("a",10),
+                new Movie("b",20),
+                new Movie("c",30)
+        );
+        var result=movies.stream().findFirst().get();
+        System.out.println(result.getName());
+
+        var max=movies
+                .stream()
+                .max(Comparator.comparing(Movie::getLikes))
+                .get();
+        System.out.println(max.getName());
+        Predicate<Movie> greaterThan20= m->m.getLikes()>20;
+        var min= movies
+                .stream()
+                .min(Comparator.comparing(Movie::getLikes))
+                .get();
+        System.out.println(min.getName());
+        var anyMatch= movies
+                .stream()
+                .anyMatch(greaterThan20);
+
+        var allMatch= movies
+                .stream()
+                .allMatch(greaterThan20);// Terminal operation
+
+        var noneMatch= movies
+                .stream()
+                .noneMatch(greaterThan20);
+
+        //You can test this out using the print method
+    }
+
+
 }

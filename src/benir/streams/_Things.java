7611198a621d2538a61fe6af93704cdce3bf9004
Benir2005 <beniromenda@gmail.com>
2025-04-06 10:26:48 +0300

    Review on Streams API

--- src/benir/streams/_Things.java
+++ src/benir/streams/_Things.java
@@ -28,7 +28,7 @@ public class _Things {
 //        _Things.filter();
 //        _Things._listOf();
 //        _Things.streamSlice();
-//        _Things.streamSort();
+        _Things.streamSort();
 //        _Things.peekElements();
 //        _Things.uniqueElements();
 //        _Things.groupGenres();
@@ -134,7 +134,8 @@ public class _Things {
         );
         movies.stream()
                 .sorted(Comparator.comparing(Movie::getName).reversed())
-                .forEach(n-> System.out.println(n.getName()));
+                .map(n->n.getName())
+                .forEach(n-> System.out.println(n));
     }
     public static void uniqueElements(){
         var movies = List.of(

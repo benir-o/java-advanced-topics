f7f00dff1d4c69699999ca9ba33a710f95d10fa6
benir-o <beniromenda@gmail.com>
2025-04-25 22:04:25 +0300

    add electricity billing system

--- src/benir/electricity_billing_system/Main.java
+++ src/benir/electricity_billing_system/Main.java
@@ -2,6 +2,7 @@ package benir.electricity_billing_system;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Welcome to the quantum grid project");
 
     }
 }

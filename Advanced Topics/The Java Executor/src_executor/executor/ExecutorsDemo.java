6f26f1dc37136c119b1228648cbcc7c8f23db64c
benir-o <beniromenda@gmail.com>
2025-05-28 21:43:53 +0300

    Thread manipulation

--- Advanced Topics/The Java Executor/src_executor/executor/ExecutorsDemo.java
+++ Advanced Topics/The Java Executor/src_executor/executor/ExecutorsDemo.java
@@ -1,16 +1,24 @@
 package executor;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ExecutorsDemo {
     public static void show(){
+        //Don't create an executor class immediately
+        //The constructor has many parameters to pass
+        //Executors.newSingleThreadExecutor();
         /*
-        Executing a task on a single thread
+        This returns a single executor thread
+        scheduledThreadPool->returns an instance of the
+        scheduled threadpool executor
          */
-        ExecutorService executor = Executors.newFixedThreadPool(2);
+        var executor=Executors.newFixedThreadPool(2);
+        //creating an executor that contains 2 threads.
+        /*
+        We can then submit a task to the threadPool we have created
+         */
 
-        try {
+        try{
             executor.submit(()->{
                 System.out.println(Thread.currentThread().getName());
             });
@@ -18,9 +26,28 @@ public class ExecutorsDemo {
             executor.shutdown();
         }
 
+        /*
+        When we start an executor and submit a task, the
+        executor thinks there might be more tasks coming in the future.
+        So executor stays in memory waiting for new tasks.
+
+        we should call shutdown.
+        The method Waits for completion of current tasks without allowing new
+        tasks to be let in.
+
+        At times, we may run into an error before calling executor.shutdown, so it is necessary
+        we wrap the executors.submit() in a try finally block.
+
+        While the executor framework handles thread manipulation for us,
+        it does not prevent the programmer from encountering race conditions.
+
+         */
 
 
 
     }
-}
 
+    public static void main(String[] args) {
+        show();
+    }
+}
--- Advanced Topics/The Java Executor/src_executor/executor/Main.java
+++
@@ -1,7 +0,0 @@
-package executor;
-
-public class Main {
-    public static void main(String[] args){
-        ExecutorsDemo.show();
-    }
-}

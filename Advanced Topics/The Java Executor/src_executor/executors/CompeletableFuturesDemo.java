40ce1b41ec51c9a0928ca7138070d78800f4bde7
benir-o <beniromenda@gmail.com>
2025-06-08 17:01:28 +0300

    Combining Completable Futures

--- Advanced Topics/The Java Executor/src_executor/executors/CompeletableFuturesDemo.java
+++ Advanced Topics/The Java Executor/src_executor/executors/CompeletableFuturesDemo.java
@@ -143,4 +143,18 @@ public class CompeletableFuturesDemo {
                 CompeletableFuturesDemo::getPlaylist
         ).thenAccept(playlist-> System.out.println(playlist));
     }
+    public static void combiningCompletableFutures(){
+        /*
+        Everything is happening asynchronously and to results are combined
+         */
+        var first=CompletableFuture
+                .supplyAsync(()->"20USD")
+                .thenApply(str->{
+                    var price=str.replace("USD","");
+                    return Integer.parseInt(price);
+                });
+        var second=CompletableFuture.supplyAsync(()->0.9);
+        first.thenCombine(second,(price,exchangeRate)->price*exchangeRate)
+                .thenAccept(result-> System.out.println(result));
+    }
 }
